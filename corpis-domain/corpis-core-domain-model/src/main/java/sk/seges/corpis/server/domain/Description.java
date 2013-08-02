d37e519f259a96c7a1a10a542ba7061703159966
sloboda@seges.sk <sloboda@seges.sk>
2013-08-02 14:47:17 +0200

    Add Name, Price, Description from branch gwt_25

--- corpis-domain/corpis-core-domain-model/src/main/java/sk/seges/corpis/server/domain/Description.java
+++ corpis-domain/corpis-core-domain-model/src/main/java/sk/seges/corpis/server/domain/Description.java
@@ -2,13 +2,13 @@ package sk.seges.corpis.server.domain;
 
 import sk.seges.corpis.appscaffold.shared.annotation.BaseObject;
 import sk.seges.corpis.appscaffold.shared.annotation.DomainInterface;
+import sk.seges.sesam.domain.IMutableDomainObject;
 
 @DomainInterface
 @BaseObject
-public interface Name {
+public interface Description extends IMutableDomainObject<Long> {
 
 	String language();
-	
 	String value();
 
 }
