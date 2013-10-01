5d30e86d746012aecf02e54813e6a411b49aa452
Juraj Smolinský <juraj.smolinsky@seges.sk>
2013-10-01 14:21:54 +0200

    - add new types od transports

--- corpis-domain/corpis-invoice-domain-model/src/main/java/sk/seges/corpis/shared/domain/invoice/ETransports.java
+++ corpis-domain/corpis-invoice-domain-model/src/main/java/sk/seges/corpis/shared/domain/invoice/ETransports.java
@@ -1,5 +1,5 @@
 package sk.seges.corpis.shared.domain.invoice;
 
 public enum ETransports {
-	DHL, UPS, FedEx, DPD, TNT, EMS, PERSONAL, POST, COD, COURIER;
+	DHL, UPS, FedEx, DPD, TNT, EMS, PERSONAL, POST, POST_COURIER, OUR_COMPANY, COD, COURIER;
 }
