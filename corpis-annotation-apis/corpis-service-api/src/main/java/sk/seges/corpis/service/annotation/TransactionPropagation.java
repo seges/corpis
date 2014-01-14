019e9f4d101e3bdbb7691706c4114b63153339e0
peter_simun <simun@seges.sk>
2014-01-14 11:07:04 +0100

    default transaction propagation is isolate

--- corpis-annotation-apis/corpis-service-api/src/main/java/sk/seges/corpis/service/annotation/TransactionPropagation.java
+++ corpis-annotation-apis/corpis-service-api/src/main/java/sk/seges/corpis/service/annotation/TransactionPropagation.java
@@ -5,7 +5,7 @@ import sk.seges.sesam.core.configuration.annotation.GenerateModel;
 @GenerateModel
 public @interface TransactionPropagation {
 
-	PropagationType value() default sk.seges.corpis.service.annotation.PropagationType.PROPAGATE;
+	PropagationType value() default sk.seges.corpis.service.annotation.PropagationType.ISOLATE;
 	PropagationTarget[] target() default { sk.seges.corpis.service.annotation.PropagationTarget.RETURN_VALUE, sk.seges.corpis.service.annotation.PropagationTarget.ARGUMENTS };
 
 	String[] fields() default {};
