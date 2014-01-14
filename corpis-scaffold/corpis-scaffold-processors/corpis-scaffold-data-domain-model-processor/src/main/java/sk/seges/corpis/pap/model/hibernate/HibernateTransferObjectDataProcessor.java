1870fedef6d8164bfbc7c0c44cca2cdabe010a9e
peter_simun <simun@seges.sk>
2014-01-14 18:33:23 +0100

    imports reorganization

--- corpis-scaffold/corpis-scaffold-processors/corpis-scaffold-data-domain-model-processor/src/main/java/sk/seges/corpis/pap/model/hibernate/HibernateTransferObjectDataProcessor.java
+++ corpis-scaffold/corpis-scaffold-processors/corpis-scaffold-data-domain-model-processor/src/main/java/sk/seges/corpis/pap/model/hibernate/HibernateTransferObjectDataProcessor.java
@@ -1,9 +1,5 @@
 package sk.seges.corpis.pap.model.hibernate;
 
-import javax.annotation.processing.SupportedSourceVersion;
-import javax.lang.model.SourceVersion;
-import javax.lang.model.element.TypeElement;
-
 import sk.seges.corpis.appscaffold.model.pap.model.DataConfigurationTypeElement;
 import sk.seges.corpis.pap.model.hibernate.provider.DataClasspathConfigurationProvider;
 import sk.seges.corpis.pap.model.hibernate.resolver.HibernateEntityResolver;
@@ -12,6 +8,10 @@ import sk.seges.sesam.pap.model.model.ConfigurationContext;
 import sk.seges.sesam.pap.model.model.ConfigurationTypeElement;
 import sk.seges.sesam.pap.model.provider.api.ConfigurationProvider;
 import sk.seges.sesam.pap.model.resolver.api.EntityResolver;
+
+import javax.annotation.processing.SupportedSourceVersion;
+import javax.lang.model.SourceVersion;
+import javax.lang.model.element.TypeElement;
 
 @SupportedSourceVersion(SourceVersion.RELEASE_6)
 public class HibernateTransferObjectDataProcessor extends TransferObjectProcessor {
