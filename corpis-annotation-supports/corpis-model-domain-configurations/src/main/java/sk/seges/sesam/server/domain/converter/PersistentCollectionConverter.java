3c859a837f5e09137c9a910568d6fa5e6d82a101
peter_simun <simun@seges.sk>
2014-01-20 11:38:25 +0100

    Better handling of the hibernate proxy objects

--- corpis-annotation-supports/corpis-model-domain-configurations/src/main/java/sk/seges/sesam/server/domain/converter/PersistentCollectionConverter.java
+++ corpis-annotation-supports/corpis-model-domain-configurations/src/main/java/sk/seges/sesam/server/domain/converter/PersistentCollectionConverter.java
@@ -1,16 +1,33 @@
 package sk.seges.sesam.server.domain.converter;
 
 import org.hibernate.collection.PersistentSet;
+import org.hibernate.proxy.HibernateProxy;
 import sk.seges.sesam.server.model.converter.common.CollectionConverter;
 import sk.seges.sesam.shared.model.converter.ConverterProviderContext;
 
 import java.util.Collection;
 import java.util.HashSet;
+import java.util.Iterator;
 
 public class PersistentCollectionConverter<DTO, DOMAIN> extends CollectionConverter<DTO, DOMAIN> {
 
 	public PersistentCollectionConverter(ConverterProviderContext converterProviderContext) {
 		super(converterProviderContext);
+	}
+
+	@Override
+	protected DOMAIN getDomainObject(Iterator<?> iterator) {
+		DOMAIN result = super.getDomainObject(iterator);
+
+		if (result == null) {
+			return null;
+		}
+
+		if (result instanceof HibernateProxy) {
+			result = (DOMAIN) ((HibernateProxy) result).getHibernateLazyInitializer().getImplementation();
+		}
+
+		return result;
 	}
 
 	@Override
