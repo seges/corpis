9e6537f12cb074d6447bafdd9ce1cc2affc8474c
Jaroslav Smolinský <jaroslav.smolinsky@seges.sk>
2014-04-22 16:51:07 +0200

    Feature #7203 Olea settings - pri zmene obchodaka treba premigrovat aj ACLka na noveho

--- corpis-domain-jpa/corpis-invoice-domain-jpa/src/main/java/sk/seges/corpis/server/domain/invoice/jpa/JpaOrderCore.java
+++ corpis-domain-jpa/corpis-invoice-domain-jpa/src/main/java/sk/seges/corpis/server/domain/invoice/jpa/JpaOrderCore.java
@@ -35,6 +35,8 @@ public abstract class JpaOrderCore extends OrderCoreBase implements OrderCoreDat
 	public static final String CUSTOMER_PERSON_NAME = "customerPersonName";
 	public static final String CUSTOMER_COMPANY_NAME = "customerCompanyName";
 	
+	public static final String CUSTOMER_ID_COLUMN_NAME = "customer_id";
+
 	public JpaOrderCore() {
 		setDeliveryContact(new JpaBasicContact());
 		setDeliveryPerson(new JpaDeliveryPerson());
@@ -46,52 +48,62 @@ public abstract class JpaOrderCore extends OrderCoreBase implements OrderCoreDat
 		setCustomerCompanyName(new JpaCompanyName());
 	}
 
+	@Override
 	@Column(name = "creation_date")
 	public Date getCreationDate() {
 		return super.getCreationDate();
 	}
 
+	@Override
 	@ManyToOne
 	public JpaCurrency getCurrency() {
 		return (JpaCurrency) super.getCurrency();
 	}
 
+	@Override
 	@Column
 	public String getOrderId() {
 		return super.getOrderId();
 	}
 
+	@Override
 	@Column(name = "note")
 	public String getNote() {
 		return super.getNote();
 	}
 
+	@Override
 	@Column(name = "delivered_by")
 	public ETransports getDeliveredBy() {
 		return super.getDeliveredBy();
 	}
 
+	@Override
 	@Deprecated
 	@Column(name = "delivery_price")
 	public Double getDeliveryPrice() {
 		return super.getDeliveryPrice();
 	}
 
+	@Override
 	@Column(name = "tracking_number")
 	public String getTrackingNumber() {
 		return super.getTrackingNumber();
 	}
 
+	@Override
 	@ManyToOne(targetEntity = JpaOrderStatus.class)
 	public OrderStatusData getStatus() {
 		return super.getStatus();
 	}
 
+	@Override
 	@Column(name = "ico")
 	public String getIco() {
 		return super.getIco();
 	}
 
+	@Override
 	@Column(name = "icdph")
 	public String getIcDph() {
 		return super.getIcDph();
@@ -103,21 +115,25 @@ public abstract class JpaOrderCore extends OrderCoreBase implements OrderCoreDat
 		return super.getDic();
 	}
 
+	@Override
 	@Column
 	public EPaymentType getPaymentType() {
 		return super.getPaymentType();
 	}
 
+	@Override
 	@Column(name = "account_number")
 	public String getAccountNumber() {
 		return super.getAccountNumber();
 	}
 
+	@Override
 	@Column(name = "project_number")
 	public String getProjectNumber() {
 		return super.getProjectNumber();
 	}
 
+	@Override
 	@Column(name = "same_delivery_address")
 	public Boolean getSameDeliveryAddress() {
 		return super.getSameDeliveryAddress();
@@ -129,6 +145,7 @@ public abstract class JpaOrderCore extends OrderCoreBase implements OrderCoreDat
 		return super.getUserName();
 	}
 
+	@Override
 	public Date getUpdatedDate() {
 		return super.getUpdatedDate();
 	}
