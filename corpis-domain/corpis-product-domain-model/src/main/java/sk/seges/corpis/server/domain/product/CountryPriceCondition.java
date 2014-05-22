05273af0c2e86bbea891738b65b81fb45bca93e5
Peter Sloboda <sloboda@seges.sk>
2014-05-22 08:17:59 +0200

    Remove ExplicitValuePriceCondition from CountryPriceCondition

--- corpis-domain/corpis-product-domain-model/src/main/java/sk/seges/corpis/server/domain/product/CountryPriceCondition.java
+++ corpis-domain/corpis-product-domain-model/src/main/java/sk/seges/corpis/server/domain/product/CountryPriceCondition.java
@@ -1,10 +1,9 @@
 package sk.seges.corpis.server.domain.product;
 
 import sk.seges.corpis.appscaffold.shared.annotation.DomainInterface;
-import sk.seges.corpis.shared.domain.price.api.ExplicitValuePriceCondition;
 
 @DomainInterface
-public interface CountryPriceCondition extends PriceCondition, ExplicitValuePriceCondition {
+public interface CountryPriceCondition extends PriceCondition {
 
 	String countryCode();
 
