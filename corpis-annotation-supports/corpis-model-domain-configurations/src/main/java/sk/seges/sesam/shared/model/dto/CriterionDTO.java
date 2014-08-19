38e487efdef4df8e4cd28fac1c49502efc946b50
Juraj Smolinský <juraj.smolinsky@seges.sk>
2014-08-19 23:16:04 +0200

    #7527 Palac motesickych - naplnenie webu

--- corpis-annotation-supports/corpis-model-domain-configurations/src/main/java/sk/seges/sesam/shared/model/dto/CriterionDTO.java
+++ corpis-annotation-supports/corpis-model-domain-configurations/src/main/java/sk/seges/sesam/shared/model/dto/CriterionDTO.java
@@ -2,6 +2,13 @@ package sk.seges.sesam.shared.model.dto;
 
 import java.io.Serializable;
 
+import org.codehaus.jackson.annotate.JsonSubTypes;
+import org.codehaus.jackson.annotate.JsonSubTypes.Type;
+import org.codehaus.jackson.annotate.JsonTypeInfo;
+import org.codehaus.jackson.annotate.JsonTypeInfo.As;
+
+@JsonTypeInfo(use = JsonTypeInfo.Id.MINIMAL_CLASS, include = As.PROPERTY, property = "className")
+@JsonSubTypes({ @Type(value = SimpleExpressionDTO.class, name = "simple"), @Type(value = JunctionDTO.class, name = "junction") })
 public interface CriterionDTO extends Serializable {
 
 	/** Hibernate's Restrictions method name counterpart. */
